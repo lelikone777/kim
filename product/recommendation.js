cb4ace223f14d7f9fae1b5e234523d6c923e9b74
lelikone777 <2257855@gmail.com>
2023-11-22 18:54:24 +0400

    recommendation section hotfix

--- product/recommendation.js
+++ product/recommendation.js
@@ -14,104 +14,128 @@ recommendation.innerHTML = `
     
     
     
-      <div class='flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
-        <div class='flex flex-col'>
-          <div class=' flex justify-center items-center w-12 h-12 border border-semi-grey shrink-0 relative'>
-            <div class='absolute border border-semi-grey rounded-full bg-light w-2 h-2 bottom-[-4px] left-1/2 translate-x-[-50%]'></div>
-            <div class='absolute border border-primary rounded-full bg-transparent w-1 h-1 bottom-[-2px] left-1/2 translate-x-[-50%]'></div>
-            <span class='text-26-300'>1</span>
-          </div>
-          <div class='relative w-full h-[100%] '>
-            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
-          </div>
+<!--      1-->
+  <div class='xl:flex '>
+    <div class='flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
+      <div class='flex flex-col xl:flex-row'>
+      
+        <div class=' flex justify-center items-center w-12 h-12 xl:w-16 xl:h-16 border border-semi-grey shrink-0 relative'>
+          <div class='absolute border border-semi-grey rounded-full bg-light z-10 w-2 h-2 xl:w-3 xl:h-3 bottom-[-8px] xl:bottom-auto left-1/2 xl:left-auto xl:right-[-6px] translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%]'></div>
+          <div class='absolute border border-primary rounded-full bg-transparent z-20 w-1 h-1 bottom-[-4px] xl:bottom-auto left-1/2 xl:left-auto translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%] xl:right-[-2px]'></div>
+          <span class='text-26-300'>1</span>
         </div>
-        <div class='text-14-300 md:flex md:items-center'>
-          Определитесь с локацией, к которой вы хотите привлечь внимание в интерьере, или подумайте, как именно вы хотите зонировать пространство
+        
+        <div class='relative w-full h-[100%] '>
+          <div class='absolute w-[1px] xl:w-full h-[100%] xl:h-[1%] bottom-0 xl:bottom-[50%] left-[50%] xl:left-0 translate-x-[-50%] xl:translate-x-0 xl:translate-y-[-50%] bg-semi-grey'></div>
         </div>
+        
       </div>
       
-      <div class=' flex justify-between gap-x-4'>
-        <div class='flex justify-center items-center w-12 h-3 shrink-0 relative'>
-          <div class='relative w-full h-[100%] '>
-            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
-          </div>
-        </div>
-        <div class=''></div>
+      <div class='text-14-20-300 flex items-center xl:pr-10'>
+        Определитесь с локацией, к которой вы хотите привлечь внимание в интерьере, или подумайте, как именно вы хотите зонировать пространство
       </div>
       
-      <div class=' flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
-        <div class='flex flex-col'>
-          <div class=' flex justify-center items-center w-12 h-12 border border-semi-grey shrink-0 relative'>
-            <div class='absolute border border-semi-grey rounded-full bg-light w-2 h-2 bottom-[-4px] left-1/2 translate-x-[-50%]'></div>
-            <div class='absolute border border-primary rounded-full bg-transparent w-1 h-1 bottom-[-2px] left-1/2 translate-x-[-50%]'></div>
-            <span class='text-26-300'>2</span>
-          </div>
-          <div class='relative w-full h-[100%] '>
-            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
-          </div>
-        </div>
-        <div class='text-14-300 md:flex md:items-center'>
-          Создайте из рыбок композицию, используя нашу памятку
-        </div>
-      </div>
-      
-      <div class=' flex justify-between gap-x-4'>
-        <div class='flex justify-center items-center w-12 h-3 shrink-0 relative'>
-          <div class='relative w-full h-[100%] '>
-            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
-          </div>
-        </div>
-        <div class=''></div>
-      </div>
-      
-      <div class=' flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
-        <div class='flex flex-col'>
-          <div class=' flex justify-center items-center w-12 h-12 border border-semi-grey shrink-0 relative'>
-            <div class='absolute border border-semi-grey rounded-full bg-light w-2 h-2 bottom-[-4px] left-1/2 translate-x-[-50%]'></div>
-            <div class='absolute border border-primary rounded-full bg-transparent w-1 h-1 bottom-[-2px] left-1/2 translate-x-[-50%]'></div>
-            <span class='text-26-300'>3</span>
-          </div>
-          <div class='relative w-full h-[100%] '>
-            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
-          </div>
-        </div>
-        <div class='text-14-300 md:flex md:items-center'>
-          Потратьте по 7 минут на монтаж каждой рыбки
-        </div>
-      </div>
-      
-      <div class=' flex justify-between gap-x-4'>
-        <div class='flex justify-center items-center w-12 h-3 shrink-0 relative'>
-          <div class='relative w-full h-[100%] '>
-            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
-          </div>
-        </div>
-        <div class=''></div>
-      </div>
-      
-      <div class=' flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
-        <div class='flex flex-col'>
-          <div class=' flex justify-center items-center w-12 h-12 border border-semi-grey shrink-0 relative'>
-            <div class='absolute border border-semi-grey rounded-full bg-light w-2 h-2 bottom-[-4px] left-1/2 translate-x-[-50%]'></div>
-            <div class='absolute border border-primary rounded-full bg-transparent w-1 h-1 bottom-[-2px] left-1/2 translate-x-[-50%]'></div>
-            <span class='text-26-300'>4</span>
-          </div>
-        </div>
-        <div class='text-14-300 md:flex md:items-center'>
-          Наслаждайтесь результатом!
-        </div>
-      </div>
-      
-
-      
-
-    
     </div>
     
+    <div class='line-flop flex justify-between gap-x-4 xl:hidden'>
+      <div class='flex justify-center items-center w-12 h-3 shrink-0 relative'>
+        <div class='relative w-full h-[100%] '>
+          <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
+        </div>
+      </div>
+    </div>
+  </div>
+        
+      
+<!--      2-->
+  <div class='xl:flex '>
+    <div class='flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
+      <div class='flex flex-col xl:flex-row'>
+      
+        <div class=' flex justify-center items-center w-12 h-12 xl:w-16 xl:h-16 border border-semi-grey shrink-0 relative'>
+          <div class='absolute border border-semi-grey rounded-full bg-light z-10 w-2 h-2 xl:w-3 xl:h-3 bottom-[-8px] xl:bottom-auto left-1/2 xl:left-auto xl:right-[-6px] translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%]'></div>
+          <div class='absolute border border-primary rounded-full bg-transparent z-20 w-1 h-1 bottom-[-4px] xl:bottom-auto left-1/2 xl:left-auto translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%] xl:right-[-2px]'></div>
+          <span class='text-26-300'>2</span>
+        </div>
+        
+        <div class='relative w-full h-[100%] '>
+          <div class='absolute w-[1px] xl:w-full h-[100%] xl:h-[1%] bottom-0 xl:bottom-[50%] left-[50%] xl:left-0 translate-x-[-50%] xl:translate-x-0 xl:translate-y-[-50%] bg-semi-grey'></div>
+        </div>
+        
+      </div>
+        
+      <div class='text-14-20-300 flex items-center xl:pr-10'>
+        Создайте из рыбок композицию, используя&nbsp;нашу памятку
+      </div>
+        
+      </div>
+      
+      <div class='line-flop flex justify-between gap-x-4 xl:hidden'>
+        <div class='flex justify-center items-center w-12 h-3 shrink-0 relative'>
+          <div class='relative w-full h-[100%] '>
+            <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+
+      
+<!--      3-->
+  <div class='xl:flex '>
+    <div class='flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
+      <div class='flex flex-col xl:flex-row'>
+      
+        <div class=' flex justify-center items-center w-12 h-12 xl:w-16 xl:h-16 border border-semi-grey shrink-0 relative'>
+          <div class='absolute border border-semi-grey rounded-full bg-light z-10 w-2 h-2 xl:w-3 xl:h-3 bottom-[-8px] xl:bottom-auto left-1/2 xl:left-auto xl:right-[-6px] translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%]'></div>
+          <div class='absolute border border-primary rounded-full bg-transparent z-20 w-1 h-1 bottom-[-4px] xl:bottom-auto left-1/2 xl:left-auto translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%] xl:right-[-2px]'></div>
+          <span class='text-26-300'>3</span>
+        </div>
+        
+        <div class='relative w-full h-[100%] '>
+          <div class='absolute w-[1px] xl:w-full h-[100%] xl:h-[1%] bottom-0 xl:bottom-[50%] left-[50%] xl:left-0 translate-x-[-50%] xl:translate-x-0 xl:translate-y-[-50%] bg-semi-grey'></div>
+        </div>
+        
+      </div>
+      
+      <div class='text-14-20-300 flex items-center xl:pr-11'>
+        Потратьте по 7 минут на монтаж каждой рыбки
+      </div>
+      
+    </div>
+    
+    <div class='line-flop flex justify-between gap-x-4 xl:hidden'>
+      <div class='flex justify-center items-center w-12 h-3 shrink-0 relative'>
+        <div class='relative w-full h-[100%] '>
+          <div class='absolute w-[1px] h-[100%] bottom-0 left-[50%] translate-x-[-50%] bg-semi-grey'></div>
+        </div>
+      </div>
+    </div>
+  </div>
     
     
     
+    <!--      4-->
+      <div class='xl:flex '>
+        <div class='flex justify-between gap-x-4 xl:flex-col gap-y-4 flex-1'>
+          <div class='flex flex-col xl:flex-row'>
+          
+            <div class=' flex justify-center items-center w-12 h-12 xl:w-16 xl:h-16 border border-semi-grey shrink-0 relative'>
+              <div class='absolute border border-semi-grey rounded-full bg-light z-10 w-2 h-2 xl:w-3 xl:h-3 bottom-[-8px] xl:bottom-auto left-1/2 xl:left-auto xl:right-[-6px] translate-x-[-50%] xl:translate-x-0 xl:top-1/2 translate-y-[-50%]'>            
+              </div>
+              <span class='text-26-300'>4</span>
+              
+              <svg class='absolute bottom-[-2px] left-1/2 translate-x-[-50%] w-1 h-1 xl:w-2 xl:h-2 z-20 xl:bottom-auto xl:left-auto xl:translate-x-0 xl:top-1/2 xl:right-[-2px] xl:right-[-4px] xl:translate-y-[-50%]' xmlns="http://www.w3.org/2000/svg" width="8" height="7" viewBox="0 0 8 7" fill="none">
+                <path d="M0.390625 3.49998L2.79803 5.66665L7.61285 1.33331" stroke="#033260"/>
+              </svg>
+            </div>
     
-    
+          </div>
+          
+          <div class='text-14-20-300 flex items-center xl:pr-10'> Наслаждайтесь результатом!</div>
+          
+        </div>
+      </div>
+    </div>
   </div>  
 `
