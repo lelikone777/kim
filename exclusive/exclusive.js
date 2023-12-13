0aea9f293eca89b84b8d3463859d5ec2386f1016
lelikone777 <2257855@gmail.com>
2023-12-13 15:41:13 +0400

    demo page3

--- exclusive/exclusive.js
+++ exclusive/exclusive.js
@@ -4,7 +4,7 @@ import { headerPrimary } from '../src/components/header/headerPrimary.js'
 import { mainFooter } from '../src/components/footer/footer.js'
 import { exclusiveTop } from './top.js'
 import { exclusiveList } from './exclusiveList.js'
-import { contact } from '../mainPage/contact.js'
+// import { contact } from '../mainPage/contact.js'
 import { exclusivePrice } from './exclusivePrice.js'
 
 const exclusive = document.querySelector('#exclusive')
